9b0e6caa4752d0a6f2b92fd99da50b2987c5dd7c
Black-Diamond2006 <abdulbariy.iphone.nasirov@gmail.com>
2022-08-23 12:01:38 -0700

    first commit

---
+++ css/main.js
@@ -0,0 +1,32 @@
+// let btn = document.getElementsByClassName('purchase');
+// let item = document.getElementById('order-wrapper');
+
+// btn.addEventListener('click', () =>{
+//   if(item.style.display == 'none') {
+//     item.style.display == 'block'
+//   }
+//   else {
+//     item.style.display == 'none'
+//   }
+// })
+
+    document.querySelectorAll('.purchase').forEach(header => {
+    header.addEventListener('click', function open() {
+      this.parentElement.querySelector('.order-wrapper').classList.toggle('active')
+  })
+})
+
+// function openSidebar() {
+//   let x = document.getElementsByClassName('sidebar');
+//   x.classList.add('active')
+// }
+
+
+
+var toggleButton = document.querySelector('.toggle-menu');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+	navBar.classList.toggle('toggle');
+});
+
+
